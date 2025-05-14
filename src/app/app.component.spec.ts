a442b50d970610c77a260186b2b7bcd51d369b1b
zFranco <francoheflo@gmail.com>
2025-05-13 20:23:37 -0500

    feat: change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-podedex-app' title`, () => {
+  it(`should have the 'angular-pokedex-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-podedex-app');
+    expect(app.title).toEqual('angular-pokedex-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-podedex-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-pokedex-app'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'angular-podedex-app';
+  title = 'angular-pokedex-app';
 }
